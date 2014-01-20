98e48c3bab4a5a85d7258b7b70b62c90b9675158
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2014-01-20 00:59:06 -0500

    Nicer example

--- examples/double-well-scan.py
+++ examples/double-well-scan.py
@@ -9,7 +9,7 @@ from fourier_DVR_1D import Domain_Fourier_DVR_1D
 # settings
 m0 = 1.0
 m1 = 5.0
-n_lam = 100
+n_m = 100
 x_min = -5.0
 x_max = 5.0
 n_DVR = 400
@@ -19,15 +19,10 @@ D0 = 50.0
 a = 1.5
 scale = 1.5
 V = lambda x: (D0 / a**4) * (x**2 - a**2)**2
+E_max = 2 * D0
 
 
-def m_l(lam, m0, m1):
-    """Mass switching function."""
-
-    return m0 * m1 / (lam * np.sqrt(m0) + (1-lam) * np.sqrt(m1))**2
-
-
-m = [m_l(lam, m0, m1) for lam in np.linspace(0, 1, n_lam)]
+m = np.linspace(m0, m1, n_m)
 x = np.linspace(x_min, x_max, n_g)
 V_x = V(x)
 
@@ -35,30 +30,30 @@ V_x = V(x)
 domain = Domain_Fourier_DVR_1D(x_min, x_max, n_DVR)
 
 # calculate eigenenergies and eigenstates for all masses
+print 'calculate'
 E = []
-E_four = []
 for i in range(len(m)):
 
     # solve
-    e, e_four = domain.solve(m[i], V)
+    e = domain.solve(m[i], V, n_states=n_plot, calc_eigenstates=False)
     E.append(e)
-    E_four.append(e_four)
 
     # print progress
-    print '%6.2f' % (100.0 * i / n_lam)
-
+    print '%6.2f %%' % (100.0 * (i+1) / n_m)
 E = np.array(E)
+print
 
-for i in range(len(m)):
+print 'plot'
+for i in range(n_m):
 
     plt.figure(figsize=(10, 6))
 
     plt.subplot(121)
-    plt.plot([m[i], m[i]], [0, 2*D0], 'k--', color='gray')
+    plt.plot([m[i], m[i]], [0, E_max], 'k--', color='gray')
     for j in range(n_plot):
         plt.plot(m, E[:,j], '-')
     plt.xlim(m0, m1)
-    plt.ylim(0, 2*D0)
+    plt.ylim(0, E_max)
     plt.xlabel('mass')
     plt.ylabel('energy')
 
@@ -69,13 +64,14 @@ for i in range(len(m)):
     plt.ylabel('energy')
     for j in range(n_plot):
         E_x = E[i, j] * np.ones_like(x)
-        E_x /= E_x > V_x
+        E_x[E_x < V_x] = None
         plt.plot(x, E_x)
     plt.plot(x, V_x, 'k-', lw=2)
     plt.xlim(-3, 3)
-    plt.ylim(0, 2*D0)
+    plt.ylim(0, E_max)
 
     plt.tight_layout()
 
-    plt.savefig('%03d.png' % i, dpi=75)
-    print i
+    plt.savefig('%03d.png' % i, dpi=150)
+    plt.close()
+    print '%6.2f %%' % (100.0 * (i+1) / n_m)
